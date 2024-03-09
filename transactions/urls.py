2bb491ddb2d678c4a003bd0fd093c8db776e7603
Yolshin <a.elshinv@consaltica.ru>
2024-03-09 17:35:30 +0530

    change urls for Transaction

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,9 +5,9 @@ from .views import create_transfer_transaction
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("list", views.index, name="index"),
-    path("create", views.create_transaction, name="create"),
-    path("create/income", views.create_income_transaction, name="create_income_transaction"),
-    path("create/transfer", create_transfer_transaction, name="create_transfer_transaction"),
+    path("transaction/list", views.index, name="index"),
+    path("transaction/create/expense", views.create_transaction, name="create"),
+    path("transaction/create/income", views.create_income_transaction, name="create_income_transaction"),
+    path("transaction/create/transfer", create_transfer_transaction, name="create_transfer_transaction"),
     path("settings", views.settings, name="settings")
 ]
