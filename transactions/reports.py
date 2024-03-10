8bfc9017b3ad90547c55ed755928baa0509e10a6
Yolshin <a.elshinv@consaltica.ru>
2024-03-11 01:09:49 +0530

    bugfix report for transaction home page

--- transactions/reports.py
+++ transactions/reports.py
@@ -1,11 +1,11 @@
 from typing import TYPE_CHECKING
 from datetime import datetime, timedelta
 
-from django.db.models import Sum, When, Case, DecimalField, F, Value
+from django.db.models import Sum, When, Case, DecimalField, F, Value, Q
 from django.db.models.functions import TruncDay
 from django.utils.timezone import get_default_timezone
 
-from transactions.models import Transaction, TransactionTypeEnum, TransactionType
+from transactions.models import Transaction, TransactionTypeEnum, TransactionType, ProjectUser, Account
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import User
@@ -23,15 +23,18 @@ def get_balance(owner: "User" = None):
 
     end_date = datetime.now(tz=default_timezone)
     start_date = datetime(end_date.year, end_date.month, 1, tzinfo=default_timezone)
+    project = ProjectUser.find_project_by_user(owner)
+    account_id = Account.get_default_id(project)
     expense_type = TransactionType.find_by_code(TransactionTypeEnum.EXPENSE.value)
-    income_type = TransactionType.find_by_code(TransactionTypeEnum.INCOME.value)
     result = Transaction.objects.filter(
-        owner=owner,
+        project=project,
         created_at__gte=start_date,  # Учитываем только транзакции, созданные после начальной даты
         created_at__lte=end_date  # Учитываем только транзакции, созданные до конечной даты
+    ).filter(
+        Q(expense_account_id=account_id) | Q(income_account_id=account_id)
     ).aggregate(
         total_expenses=Sum(Case(When(type=expense_type, then=F('expense_amount')), default=Value(0), output_field=DecimalField())),
-        total_income=Sum(Case(When(type=income_type, then=F('income_amount')), default=Value(0), output_field=DecimalField())),
+        total_income=Sum('income_amount'),
     )
     # Получаем значения суммы расходов и суммы доходов из результата агрегации
     total_expenses = result['total_expenses'] or 0
@@ -56,10 +59,13 @@ def get_expenses_by_day(owner: "User" = None):
     # Вычисляем начальную и конечную даты для последней недели
     end_date = datetime.now(tz=default_timezone)
     start_date = end_date - timedelta(days=7)
+    project = ProjectUser.find_project_by_user(owner)
+    account_id = Account.get_default_id(project)
 
     # Выполняем запрос на агрегацию данных
     expenses_by_day = Transaction.objects.filter(
-        owner=owner,
+        project=project,
+        expense_account_id=account_id,
         type=expense_type,  # Фильтруем только расходы
         created_at__gte=start_date,  # Учитываем только транзакции, созданные после начальной даты
         created_at__lte=end_date  # Учитываем только транзакции, созданные до конечной даты
@@ -82,20 +88,22 @@ def get_expenses_by_category(owner: "User" = None):
             "data": [100000, 80000, 50000]
         }
 
-    expense_type = TransactionType.find_by_code(TransactionTypeEnum.EXPENSE.value)
-
     end_date = datetime.now(tz=default_timezone)
     start_date = datetime(end_date.year, end_date.month, 1, tzinfo=default_timezone)
+    project = ProjectUser.find_project_by_user(owner)
+    account_id = Account.get_default_id(project)
+    expense_type = TransactionType.find_by_code(TransactionTypeEnum.EXPENSE.value)
 
     # Выполняем запрос на агрегацию данных
-    expenses_by_category = Transaction.objects.filter(
-        owner=owner,
+    expenses_by_category = (Transaction.objects.filter(
+        project=project,
+        expense_account_id=account_id,
         type=expense_type,  # Фильтруем только расходы
         created_at__date__gte=start_date,  # Учитываем только транзакции, созданные после начальной даты
         created_at__date__lte=end_date  # Учитываем только транзакции, созданные до конечной даты
     ).values('category__name').annotate(
         total_expenses=Sum('expense_amount')  # Вычисляем сумму расходов для каждой категории
-    ).order_by('category__name')
+    ).order_by('category__name'))
 
     return {
         "labels": [item['category__name'] for item in expenses_by_category],
