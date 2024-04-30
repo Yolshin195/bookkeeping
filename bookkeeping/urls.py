61650d3fcbb170096b52f26cb501a2f3e8910d84
Yolshin195 <elshin195@gmail.com>
2024-04-30 23:06:43 +0700

    Added multilanguage

--- bookkeeping/urls.py
+++ bookkeeping/urls.py
@@ -14,13 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
+]
+
+urlpatterns += i18n_patterns(
     path("", include("transactions.urls")),
     path("budget/", include("budget.urls")),
     path("users/", include("users.urls")),
-    path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls"))
-]
+)
